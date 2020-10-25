8621c38dbdd93b1b21b7dcd09ad9347920668df2
unknown <mohsinsait23@gmail.com>
2020-10-25 16:35:02 +0530

    removeKFromList

---
+++ removefromlist.py
@@ -0,0 +1,15 @@
+def removeKFromList(l, k):
+	current = l
+	prev = None
+
+	while current != None:
+		if current.value == k:
+			if prev == None:
+				l = current.next
+				current = l
+			else:
+				prev.next = current.next
+				current = current.next
+		else:
+			prev = current
+			current = current.next
